b06296819d5ab11d616c7a9f5e67e9450eee6331
aboubacar <sidikinabe10@gmail.com>
2023-10-05 15:22:52 +0200

    #14

--- flyinn/src/components/findHome/index.tsx
+++ flyinn/src/components/findHome/index.tsx
@@ -48,7 +48,7 @@ const FindHomeContainer: React.FC<IFindHomeContainer> = ({ children }) => {
           </Link>
         </div>
       </div>
-      <div className="navScreen-body">
+      <div className="navScreen-body grid grid-cols-1 gap-1">
         {children}
       </div>
       <div className="navScreen-footer">
