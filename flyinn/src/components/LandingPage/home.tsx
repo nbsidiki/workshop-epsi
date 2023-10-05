42d728f8ea52764a9a1f6d764eb4adcdceb12ce7
aboubacar <sidikinabe10@gmail.com>
2023-10-05 10:22:54 +0200

    #9

--- flyinn/src/components/LandingPage/home.tsx
+++ flyinn/src/components/LandingPage/home.tsx
@@ -1,31 +1,36 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
 
     return (
         <div className="flex items-center justify-end vsm:ml-10 sm:ml-10 ">
             <div className="flex flex-col bg-gray-300 p-8 rounded-2xl shadow-md w-full h-96">
-                    <div className=' flex rounded-3xl border-2 h-full  bg-white'>
-                        <p className='flex h-full text-lg'>
-                            Tu vis hors de la France et cherches un logement temporaire pour tes études ?, les habitants de la MEL vous acceuille.
-                        </p>
-                    </div>
-                    <div className=' flex w-full mt-4'>
+                <div className=' flex rounded-3xl border-2 h-full  bg-white'>
+                    <p className='flex h-full text-lg'>
+                        Tu vis hors de la France et cherches un logement temporaire pour tes études ?, les habitants de la MEL vous acceuille.
+                    </p>
+                </div>
+                <div className=' flex w-full mt-4'>
+                    <Link to={'/signin'}>
                         <button
                             type="button"
                             className="w-full hover:bg-orange-400 bg-orange-500 text-white rounded-3xl  px-4 py-2"
                         >
                             Inscription
                         </button>
-                    </div>
-                    <div className=' flex w-full mt-4'>
+                    </Link>
+                </div>
+                <div className=' flex w-full mt-4'>
+                    <Link to={'/login'}>
                         <button
                             type="button"
-                            className= "w-full hover:bg-orange-400 bg-orange-500 text-white rounded-3xl  px-4 py-2"
+                            className="w-full hover:bg-orange-400 bg-orange-500 text-white rounded-3xl  px-4 py-2"
                         >
                             connexion
                         </button>
-                    </div>
+                    </Link>
+                </div>
             </div>
         </div>
     );
